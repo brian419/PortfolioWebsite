78cfa90b2cca0aee6d656983020e7066791970d6
brian419 <json10@crimson.ua.edu>
2024-10-07 01:24:20 -0500

    removing comments

--- src/pages/api/getScores.js
+++ src/pages/api/getScores.js
@@ -1,22 +1,5 @@
 const { connectToDatabase } = require('../../database/mongodb');
 
-// module.exports = async function handler(req, res) {
-//     if (req.method === 'GET') {
-//         const db = await connectToDatabase();
-
-//         const scores = await db
-//             .collection('Scores')
-//             .find({})
-//             .sort({ score: -1 })
-//             .limit(10)
-//             .toArray();
-
-//         res.status(200).json(scores);
-//     } else {
-//         res.status(405).json({ message: 'Method not allowed' });
-//     }
-// };
-
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         const db = await connectToDatabase();
