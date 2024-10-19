d9e0079562efc295e41c4173fe3ed087f611a8bc
brian419 <json10@crimson.ua.edu>
2024-10-18 19:08:50 -0500

    trying to fix

--- src/app/gomoku-page/page.tsx
+++ src/app/gomoku-page/page.tsx
@@ -218,7 +218,7 @@ export default function GomokuPage() {
     };
 
     // function to handle dropping stones on the board
-    const handleDrop = (row, col, color) => {
+    const handleDrop = (row: number, col: number, color: "black" | "white") => {
         if (!grid[row][col] && !winner) {
             const newGrid = [...grid];
             newGrid[row][col] = color;
