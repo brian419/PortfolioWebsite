dda4783d396b00c396bbe04944cf0f58c0dd68f9
brian419 <json10@crimson.ua.edu>
2024-11-11 17:15:19 -0600

    using next.config.mjs not .js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'raw-loader',
+                },
+            ],
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
 
