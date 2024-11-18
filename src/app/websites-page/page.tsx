fc693140c86cf3fa5a11e69998d40abeced6a3fd
brian419 <json10@crimson.ua.edu>
2024-11-17 23:57:10 -0600

    added modal to websites page to load extra info about the website project

--- src/app/websites-page/page.tsx
+++ src/app/websites-page/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { motion } from 'framer-motion';
 import Image, { StaticImageData } from 'next/image';
 import uawaterskihomepageImage from '../../components/images/uawaterskihomepage.png';
@@ -13,27 +13,31 @@ interface Project {
     description: string;
     image: StaticImageData;
     link: string;
+    extraInfo: string;
 }
 
 const cardWidth = 340;
 
 const WebsitePage: React.FC = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [selectedProject, setSelectedProject] = useState<Project | null>(null);
 
     const websites: Project[] = useMemo(() => [
         {
             id: 1,
             title: "Capstone Project | Web Application for University of Alabama's Waterski Team",
-            description: "A website created by Anastasia Spencer, Brooke Boskus, Lilly Eide, and Jeongbin Son for the University of Alabama's waterski team. Built using React.js, Node.js, Next.js, Express.js, and Tailwind CSS.",
+            description: "A website created by Anastasia Spencer, Brooke Boskus, Lilly Eide, and Jeongbin Son for the University of Alabama's waterski team.",
             image: uawaterskihomepageImage,
-            link: 'https://testing-waterski-deployment.vercel.app/',
+            link: 'https://www.uawaterski.com',
+            extraInfo: "Built using React.js, Node.js, Next.js, Express.js, and Tailwind CSS, and connected to an Amazon RDS MySQL database. The website is deployed through Vercel. My contributions include implementing secure login functionality using JWT tokens, creating a fully functional 'Contact Us' form, and developing a 'Forgot Password' page with the ability to reset passwords in the database. I also designed and implemented the 'Roster' page, which allows users to view team members from the database, with search and filter capabilities for enhanced usability.",
         },
         {
             id: 2,
             title: "Code for Trees",
-            description: "Solo website project built using React.js, Next.js, Node.js, and Tailwind CSS. The website will eventually be developed into a platform where users can solve coding problems and gain points, redeemable to plant trees.",
+            description: "Solo website project for solving coding problems and gaining points to plant trees.",
             image: codefortreeshomepageImage,
             link: 'https://codefortrees.org/',
+            extraInfo: "This platform allows users to contribute to environmental sustainability while improving their coding skills. Built with React.js, Next.js, and Tailwind CSS.",
         },
         {
             id: 3,
@@ -41,6 +45,7 @@ const WebsitePage: React.FC = () => {
             description: "This is a placeholder for a future website project.",
             image: tempWebsiteImage,
             link: '/',
+            extraInfo: "Details about this project will be added in the future.",
         },
     ], []);
 
@@ -55,6 +60,23 @@ const WebsitePage: React.FC = () => {
             prevIndex === 0 ? websites.length - 1 : prevIndex - 1
         );
     };
+
+    const handleModalOpen = (project: Project) => {
+        setSelectedProject(project);
+    };
+
+    const handleModalClose = () => {
+        setSelectedProject(null);
+    };
+
+    // no scrolling in the background when modal is open
+    useEffect(() => {
+        if (selectedProject) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+    }, [selectedProject]);
 
     return (
         <div className="flex flex-col items-center justify-start md:mt-[-40px] mt-4 h-screen text-white relative overflow-hidden">
@@ -79,10 +101,11 @@ const WebsitePage: React.FC = () => {
                     transition={{ type: "spring", stiffness: 50, damping: 20 }}
                 >
                     {websites.map((project, index) => (
-                        <MemoizedProjectCard
+                        <ProjectCard
                             key={project.id}
                             project={project}
                             isActive={index === currentIndex}
+                            onClick={() => handleModalOpen(project)}
                         />
                     ))}
                 </motion.div>
@@ -109,6 +132,11 @@ const WebsitePage: React.FC = () => {
                     <DisabledButton label="Not Implemented Yet" />
                 )}
             </div>
+
+            {/* Modal */}
+            {selectedProject && (
+                <Modal project={selectedProject} onClose={handleModalClose} />
+            )}
         </div>
     );
 };
@@ -118,30 +146,66 @@ export default WebsitePage;
 interface ProjectCardProps {
     project: Project;
     isActive: boolean;
+    onClick: () => void;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ project, isActive }) => {
-    return (
-        <motion.div
-            className={`min-w-[300px] mx-8 transition-transform duration-300 ${isActive ? 'scale-105' : 'scale-90 opacity-50'
-                }`}
-        >
-            <div className="bg-gray-800 text-white rounded-lg p-6 shadow-2xl transform transition-transform duration-300 hover:scale-105">
-                <Image
-                    src={project.image}
-                    alt={project.title}
-                    width={300}
-                    height={200}
-                    className="rounded-t-lg object-cover"
-                />
-                <h2 className="text-xl font-bold mt-4 text-blue-400">{project.title}</h2>
-                <p className="mt-2 text-gray-300">{project.description}</p>
-            </div>
-        </motion.div>
-    );
-};
+const ProjectCard: React.FC<ProjectCardProps> = ({ project, isActive, onClick }) => (
+    <motion.div
+        onClick={onClick}
+        className={`min-w-[300px] mx-8 transition-transform duration-300 ${isActive ? 'scale-105' : 'scale-90 opacity-50'}`}
+    >
+        <div className="relative perspective bg-gray-800 text-white rounded-lg p-6 shadow-2xl transform transition-transform duration-300 hover:scale-105">
+            <Image
+                src={project.image}
+                alt={project.title}
+                width={300}
+                height={200}
+                className="rounded-t-lg object-cover"
+            />
+            <h2 className="text-xl font-bold mt-4 text-blue-400">{project.title}</h2>
+            <div className="border-b border-gray-400 my-4"></div>
+            <p className="mt-2 text-white">{project.description}</p>
+        </div>
+    </motion.div>
+);
 
-const MemoizedProjectCard = React.memo(ProjectCard);
+interface ModalProps {
+    project: Project;
+    onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ project, onClose }) => (
+    <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+        <div className="bg-gray-900 text-white p-6 rounded-lg max-w-lg w-full relative">
+            <button
+                onClick={onClose}
+                className="absolute top-2 right-2 text-gray-400 hover:text-gray-200"
+                aria-label="Close Modal"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            <h2 className="text-xl font-bold mt-4 text-blue-400">{project.title}</h2>
+            <div className="border-b border-gray-400 my-4"></div>
+            <div className="mt-2 text-white max-h-64 overflow-y-auto pr-2 space-y-4">
+                {project.extraInfo.split('\n').map((paragraph, idx) => (
+                    <p key={idx} className="leading-relaxed text-gray-300">
+                        {paragraph}
+                    </p>
+                ))}
+            </div>
+            <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-4 inline-block bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-500 text-center w-full"
+            >
+                Visit the Website!
+            </a>
+        </div>
+    </div>
+);
 
 interface LinkButtonProps {
     label: string;
@@ -150,19 +214,14 @@ interface LinkButtonProps {
 
 const LinkButton: React.FC<LinkButtonProps> = ({ label, href }) => (
     <div className="mx-auto flex w-full max-w-lg items-center justify-center mt-8">
-        <div className="relative z-10 flex w-full cursor-pointer items-center overflow-hidden rounded-xl border border-slate-800 p-[1.5px]">
-            <div className="animate-rotate absolute inset-0 h-full w-full rounded-full bg-[conic-gradient(#0ea5e9_20deg,transparent_120deg)]"></div>
-            <div className="relative z-20 flex w-full rounded-[0.60rem] bg-blue-600 p-2">
-                <a
-                    href={href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="bg-blue-600 hover:text-black text-white text-xs font-bold py-2 px-4 rounded w-full text-center"
-                >
-                    {label}
-                </a>
-            </div>
-        </div>
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-blue-600 hover:text-black text-white text-xs font-bold py-2 px-4 rounded w-full text-center"
+        >
+            {label}
+        </a>
     </div>
 );
 
