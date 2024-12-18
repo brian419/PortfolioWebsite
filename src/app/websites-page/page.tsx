c1bd3aabf681ee68a73bda30539da1b100be3348
brian419 <json10@crimson.ua.edu>
2024-12-17 22:19:51 -0500

    moved mp4 video to /public/videos for vercel

--- src/app/websites-page/page.tsx
+++ src/app/websites-page/page.tsx
@@ -7,7 +7,7 @@ import uawaterskihomepageImage from '../../components/images/uawaterskihomepage.
 import codefortreeshomepageImage from '../../components/images/codefortreeshomepage.png';
 import tempWebsiteImage from '../../components/images/placeholderProjects.svg';
 import snowskiImage from '../../components/images/snowskiimage.png'
-import snowSkiVideo from '../../components/videos/CS330FinalProjectRecording.mp4'
+import snowSkiVideo from '../../../public/videos/CS330FinalProjectRecording.mp4';
 
 interface Project {
     id: number;
