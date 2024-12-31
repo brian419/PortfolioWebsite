09f7f903c7e4469b8cac675bb4500c27d95f1ebc
brian419 <json10@crimson.ua.edu>
2024-12-31 01:57:41 -0600

    added more to extra info for all projects and added dangerouslysetinnerhtml to all. also added background gray for better visual for extra info main div for each project

--- src/app/websites-page/page.tsx
+++ src/app/websites-page/page.tsx
@@ -34,75 +34,112 @@ const WebsitePage: React.FC = () => {
             image: uawaterskihomepageImage,
             link: 'https://www.uawaterski.com',
             extraInfo: `
-                <h3 class="text-lg font-bold text-[#52c77a]">Technologies Used:</h3>
-                <p>Built using <strong>React.js</strong>, <strong>Node.js</strong>, <strong>Next.js</strong>, <strong>Express.js</strong>, and <strong>Tailwind CSS</strong>, and connected to an Amazon RDS MySQL database. The website is deployed through Vercel.</p>
-                <br>
-
-                <h3 class="text-lg font-bold text-[#52c77a]">My Contributions:</h3>
-                <ul class="list-disc list-inside">
-                    <li>Implementing secure login functionality using JWT tokens</li>
-                    <li>Creating a fully functional 'Contact Us' form</li>
-                    <li>Developing a 'Forgot Password' page with the ability to reset passwords in the database</li>
-                    <li>Designing and implementing the 'Roster' page, which allows users to view team members from the database, with search and filter capabilities for enhanced usability</li>
-                    <li>Adding a Super User (admin account) that can modify content directly within the website.
-                        <ul class="list-disc list-inside ml-4">
-                            <li>Currently, an admin can delete users, edit user information (including their profile picture), as well as confirm / deny account registrations made by users.</li>
-                        </ul>
-                    </li>
-                    <li>Created Officers Resources Page which houses different sections that help the officers of the water ski team.
-                        <ul class="list-decimal list-inside ml-4">
-                            <li>Implemented Manage Members section that can toggle between Roster (page) and Confirm Member Registration Page while still being on the Officer Resources Page. This allows an admin to easily have access to the member roster, allowing the admin to delete users and edit user information as mentioned previously. The admin can also switch to Confirm Member Registration which holds a list of pending users, or returns text that says there are no pending users!</li>
-                            <li>Implemented Manage Notes section for officers to save notes from their meetings directly within the website. This section of the Officer Resources page currently allows an officer to delete and add new notes, but cannot edit a note afterwards currently.
-                        </ul>
-                    </li>
-                </ul>
-                <br>
-
-                <h3 class="text-lg font-bold text-[#52c77a]">Cybersecurity Measures:</h3>
-                <ul class="list-disc list-inside">
-                    <li>Registration checks for valid email addresses associated with the University of Alabama</li>
-                    <li>Prevention of accounts using the email exploit trick (email+1@email.com)</li>
-                    <li>Verification step that sends a confirmation link in an email to the registered user</li>
-                    <li>
-                        Verification step that sends a confirmation link in an email to the admin user where admin can confirm or deny an account creation within the Officer Resources page.
-                        <ul class="list-decimal list-inside ml-4">
-                            <li>Admin is initially taken to login page where they can login to the admin account. Then I handled automatic redirection to officer resources page after they login!</li>
-                        </ul>
-                    </li>
-                </ul>
-                <hr style="border: 0; height: 1px; background: linear-gradient(to right, #38bdf8, #6366f1, #ec4899); margin-top: 2.5rem; margin-bottom: 2.5rem;">
-
-                <h3 class="text-lg font-bold text-[#52c77a]">Future Plans:</h3>
-                <ul class="list-disc list-inside">
-                    <li>For the future, one of the first things I would like to focus on for this website is to allow the admin account modify texts and images within the website. Our initial set up did not have a headless CMS in mind, so I figure it would be a nice challenge to figure out a way for the admin account to change content on the website!</li>
-                    <li>Additionally, I need to focus on finishing the Officer Resources Page.
-                        <ul class="list-decimal list-inside ml-4">
-                            <li>For the Manage Notes section, I would like to add a feature that allows the officer to edit a note after it is created.</li>
-                            <li>A meeting notes section visible for logged in users (athletes) could also be a nice touch to the website. An admin could upload a note to be viewed by member type (athlete, officer)</li>
-                            <li>Depth Chart will need to be implemented. Skier rankings for the waterski team. View the performance and rankings of each team member based on their recent performances. </li>
-                            <li>Fundraising Addresses will need to be implemented. This can pull from Alumni Contacts as well as Roster Information. Users will have to be able to put in relative contact information, possibly within edit profile page (click on PFP on top right of Navbar if you're a user)</li>
-                            <li>Alumni Contacts will need to be implemented. This will showcase contact information of Alumnis (email, phone number, relative's contact information) that can be easily accessed by officers. </li>
-                        </ul>
-                    </li>
-                </ul>
+                <div class="bg-gray-800 p-4 rounded-lg shadow-lg">
+                    <h3 class="text-lg font-bold text-[#52c77a]">Technologies Used:</h3>
+                    <p>Built using <strong>React.js</strong>, <strong>Node.js</strong>, <strong>Next.js</strong>, <strong>Express.js</strong>, and <strong>Tailwind CSS</strong>, and connected to an Amazon RDS MySQL database. The website is deployed through Vercel.</p>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">My Contributions:</h3>
+                    <ul class="list-disc list-inside">
+                        <li>Implementing secure login functionality using JWT tokens</li>
+                        <li>Creating a fully functional 'Contact Us' form</li>
+                        <li>Developing a 'Forgot Password' page with the ability to reset passwords in the database</li>
+                        <li>Designing and implementing the 'Roster' page, which allows users to view team members from the database, with search and filter capabilities for enhanced usability</li>
+                        <li>Adding a Super User (admin account) that can modify content directly within the website.
+                            <ul class="list-decimal list-inside ml-4">
+                                <li>Currently, an admin can delete users, edit user information (including their profile picture), as well as confirm / deny account registrations made by users.</li>
+                            </ul>
+                        </li>
+                        <li>Created Officers Resources Page which houses different sections that help the officers of the water ski team.
+                            <ul class="list-decimal list-inside ml-4">
+                                <li>Implemented Manage Members section that can toggle between Roster (page) and Confirm Member Registration Page while still being on the Officer Resources Page. This allows an admin to easily have access to the member roster, allowing the admin to delete users and edit user information as mentioned previously. The admin can also switch to Confirm Member Registration which holds a list of pending users, or returns text that says there are no pending users!</li>
+                                <li>Implemented Manage Notes section for officers to save notes from their meetings directly within the website. This section of the Officer Resources page currently allows an officer to delete and add new notes, but cannot edit a note afterwards currently.
+                            </ul>
+                        </li>
+                    </ul>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">Cybersecurity Measures:</h3>
+                    <ul class="list-disc list-inside">
+                        <li>Registration checks for valid email addresses associated with the University of Alabama</li>
+                        <li>Prevention of accounts using the email exploit trick (email+1@email.com)</li>
+                        <li>Verification step that sends a confirmation link in an email to the registered user</li>
+                        <li>
+                            Verification step that sends a confirmation link in an email to the admin user where admin can confirm or deny an account creation within the Officer Resources page.
+                            <ul class="list-decimal list-inside ml-4">
+                                <li>Admin is initially taken to login page where they can login to the admin account. Then I handled automatic redirection to officer resources page after they login!</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <hr style="border: 0; height: 1px; background: linear-gradient(to right, #38bdf8, #6366f1, #ec4899); margin-top: 2.5rem; margin-bottom: 2.5rem;">
+                    <h3 class="text-lg font-bold text-[#52c77a]">Future Plans:</h3>
+                    <ul class="list-disc list-inside">
+                        <li>For the future, one of the first things I would like to focus on for this website is to allow the admin account modify texts and images within the website. Our initial set up did not have a headless CMS in mind, so I figure it would be a nice challenge to figure out a way for the admin account to change content on the website!</li>
+                        <li>Additionally, I need to focus on finishing the Officer Resources Page.
+                            <ul class="list-decimal list-inside ml-4">
+                                <li>For the Manage Notes section, I would like to add a feature that allows the officer to edit a note after it is created.</li>
+                                <li>A meeting notes section visible for logged in users (athletes) could also be a nice touch to the website. An admin could upload a note to be viewed by member type (athlete, officer)</li>
+                                <li>Depth Chart will need to be implemented. Skier rankings for the waterski team. View the performance and rankings of each team member based on their recent performances. </li>
+                                <li>Fundraising Addresses will need to be implemented. This can pull from Alumni Contacts as well as Roster Information. Users will have to be able to put in relative contact information, possibly within edit profile page (click on PFP on top right of Navbar if you're a user)</li>
+                                <li>Alumni Contacts will need to be implemented. This will showcase contact information of Alumnis (email, phone number, relative's contact information) that can be easily accessed by officers. </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             `,
         },
         {
             id: 2,
+            title: "Snow Ski",
+            description: "A C# and ASP.NET Core website built for University of Alabama's CS 330 - Web Development class.",
+            image: snowskiImage,
+            link: 'https://cs330-fall2024-finalproject.azurewebsites.net/',
+            extraInfo: `
+                <div class="bg-gray-800 p-4 rounded-lg shadow-lg">
+                    <h3 class="text-lg font-bold text-[#52c77a]">Project Overview:</h3>
+                    <p>This website was built using C# and ASP.NET Core for University of Alabama's CS 330 - Web Development class at the end of the semester.</p>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">Team Members:</h3>
+                    <p>Built by Anastasia Spencer, Brooke Boskus, Collin Price, Owen McMenaman, and Jeongbin Son.</p>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">Technologies Used:</h3>
+                    <p>Deployed through Microsoft Azure, utilizing OpenAI and OpenWeather APIs.</p>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">My Contributions:</h3>
+                    <ul class="list-disc list-inside">
+                        <li>Handled login and register functionality along with email confirmation.</li>
+                        <li>Styled the initial frontend of the website.</li>
+                        <li>Improved the Chat Bot based on the code provided by Anastasia Spencer.</li>
+                        <li>Implemented cybersecurity checks similar to those in the Capstone website.</li>
+                    </ul>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">Note:</h3>
+                    <p>This website may not be accessible in the future as it was built using Azure's Student Plan and our group will not continue with this project since the class is over. Even if the website is not accessible in the future, the video of the website will always be visible!</p>
+                </div>
+            `,
+            video: snowSkiVideo,
+        },
+        {
+            id: 3,
             title: "Code for Trees",
             description: "Solo website project for solving coding problems and gaining points to plant trees.",
             image: codefortreeshomepageImage,
             link: 'https://codefortrees.org/',
-            extraInfo: "This platform allows users to contribute to environmental sustainability while improving their coding skills. Built with React.js, Next.js, and Tailwind CSS.",
-        },
-        {
-            id: 3,
-            title: "Snow Ski",
-            description: "This is a C#, .NET tech stack website built for CS 330 Web Development's Final Project.",
-            image: snowskiImage,
-            link: 'https://cs330-fall2024-finalproject.azurewebsites.net/',
-            extraInfo: "NOTE: This website may not be accessible in the future as it was built using Azure's Student Plan and our group will not continue with this project since the class is over. However, in such a case, I have immortalized our project as a video!\n Built by Anastasia Spencer, Brooke Boskus, Collin Price, Owen McMenaman, and Jeongbin Son. Deployed through Microsoft Azure. This was built using the student plan and utilizes OpenAI and OpenWeather apis. My personal contributions for this website includes handling login and register along with email confirmation, styling the initial frontend of the website, continuation of Chat Bot improvements based on the code provided by Anastasia Spencer. This website was a great experience in exploring other ways to do web development! I also added the cybersecurity checks I implemented in the Capstone website into this 330 website as well.",
-            video: snowSkiVideo,
+            extraInfo: `
+                <div class="bg-gray-800 p-4 rounded-lg shadow-lg">
+                    <h3 class="text-lg font-bold text-[#52c77a]">Project Overview:</h3>
+                    <p>This platform allows users to contribute to environmental sustainability while improving their coding skills.</p>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">Technologies Used:</h3>
+                    <p>Built with React.js, Next.js, and Tailwind CSS.</p>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">Features:</h3>
+                    <ul class="list-disc list-inside">
+                        <li>To be implemented in the future!</li>
+                    </ul>
+                    <br>
+                    <h3 class="text-lg font-bold text-[#52c77a]">My Contributions:</h3>
+                    <p>This will be a solo project where I handle all aspects of development, from frontend to backend.</p>
+                </div>
+            `,
         },
     ], []);
 
@@ -214,9 +251,9 @@ const WebsitePage: React.FC = () => {
                 {websites[currentIndex].id === 1 ? (
                     <LinkButton label="UA Waterski Team" href={websites[currentIndex].link} />
                 ) : websites[currentIndex].id === 2 ? (
-                    <LinkButton label="Code for Trees" href={websites[currentIndex].link} />
-                ) : websites[currentIndex].id === 3 ? (
                     <LinkButton label="Snow Ski" href={websites[currentIndex].link} />
+                ) : websites[currentIndex].id === 3 ? (
+                    <LinkButton label="Code for Trees" href={websites[currentIndex].link} />
                 ) : (
                     <DisabledButton label="Not Implemented Yet" />
                 )}
@@ -336,8 +373,16 @@ const Modal: React.FC<ModalProps> = ({ project, onClose, onPrev, onNext }) => (
                             className="prose prose-sm prose-invert"
                             dangerouslySetInnerHTML={{ __html: project.extraInfo }}
                         />
-                    ) : project.id === 4 ? (
-                        <div dangerouslySetInnerHTML={{ __html: project.extraInfo }} />
+                    ) : project.id === 2 ? (
+                        <div
+                            className="prose prose-sm prose-invert"
+                            dangerouslySetInnerHTML={{ __html: project.extraInfo }}
+                        />
+                    ) : project.id === 3 ? (
+                        <div
+                            className="prose prose-sm prose-invert"
+                            dangerouslySetInnerHTML={{ __html: project.extraInfo }}
+                        />
                     ) : (
                         project.extraInfo.split('\n').map((paragraph, idx) => (
                             <p key={idx} className="leading-relaxed text-gray-300">
